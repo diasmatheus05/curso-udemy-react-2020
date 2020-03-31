9dcc530471bb3cd7536d11a9cc503b6fe9b44c54
Matheus Dias <diasmatheus05@gmail.com>
2020-03-31 13:54:03 -0300

    Criação do prjeto cadastro-produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** 
+class App extends React.Component {
+  
+  state = {
+    nome : ''
+  }
+  
+  modificarNome = (event) => {
+    this.setState({
+      nome : event.target.value
+    })
+  }
+
+  criaComboBox = () => {
+    const opcoes = [ "Fulano", "Ciclano" ]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  render() {
+    return(
+      <>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1> Hello {this.state.nome} ! </h1>
+        { this.criaComboBox() }
+      </>
+    )
+  }
 }
 
+export default App;
+*/
+
+function App(props) {
+
+  //Não possui state
+
+  const modificarNome = (event) => {
+    console.log(event.target.value)
+  }
+
+  //A função deve ser uma constante 
+  const criaComboBox = () => {
+    const opcoes = [ "Fulano", "Ciclano" ]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  //Não existe componentDidMount
+
+  //Não existe metodo render
+  return(
+    <>
+      <input type="text" value={props.nome} onChange={modificarNome} />
+      <h1> Hello {props.nome} ! </h1>
+      { criaComboBox() }
+    </>
+  )
+
+}
 export default App;
